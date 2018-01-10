be8c5dc655f9ab38517a7a5f69103a1007fd9af8
Qiulin Wang <wangql.dev@gmail.com>
2018-01-09 18:43:01 -0800

    Added includes repcie java test.

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -19,6 +19,15 @@ describe 'ITNS540_demo::default' do
       expect { chef_run }.to_not raise_error
     end
 
+    # it 'includes cookbook java' do
+    #   expect_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('ITNS540_demo::java')
+    #   chef_run
+    # end
+
+    it 'includes recipe java' do
+      expect(chef_run).to include_recipe('ITNS540_demo::java')
+    end
+
     it 'installs java 8' do
       stub_command('java -version | grep 1.8').and_return(true)
     end
